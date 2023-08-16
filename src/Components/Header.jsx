ba09bae0508a38882e19b404148321102591497a
Elvis <luciphoenix.soft@gmail.com>
2023-08-16 23:47:51 +0000

    create navbar component in header,change bgc of header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,12 @@
 import './Styles/header.css'
+import NavBar from './NavBar'
 
 function Header() {
     return (
         <>
             <div className='headerBody'>
-                <h1>HELLO WORLD</h1>
+                <h1>Curriculum vitae</h1>
+                <NavBar/>
             </div>
         </>
     )
